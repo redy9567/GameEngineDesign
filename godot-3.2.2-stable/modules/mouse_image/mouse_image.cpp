b0f1960ddf1bc967b27baf45361539a9088df9d2
Brandon L'Abbe <brandon.labbe@mymail.champlain.edu>
2020-09-18 14:05:52 -0400

    project 1

--- godot-3.2.2-stable/modules/mouse_image/mouse_image.cpp
+++ godot-3.2.2-stable/modules/mouse_image/mouse_image.cpp
@@ -4,17 +4,10 @@ MouseImage::MouseImage() {
 
 	x = 0;
 	y = 0;
+	mPath = "";
 }
 
 MouseImage::~MouseImage() {
-}
-
-void MouseImage::setX(int val) {
-	x = val;
-}
-
-void MouseImage::setY(int val) {
-	y = val;
 }
 
 int MouseImage::getX() {
@@ -25,11 +18,51 @@ int MouseImage::getY() {
 	return y;
 }
 
+void MouseImage::setLocation(int x, int y) {
+
+	this->x = x;
+	this->y = y;
+}
+
+void MouseImage::setImagePath(String path) {
+
+	mPath = path;
+}
+
+String MouseImage::getPath() {
+	return mPath;
+}
+
+void MouseImage::updateImage() {
+
+	printf("Update Image");
+	set_texture(ResourceLoader::load(mPath));
+}
+
 void MouseImage::_bind_methods() {
 
-	ClassDB::bind_method(D_METHOD("setX", "value"), &MouseImage::setX);
-	ClassDB::bind_method(D_METHOD("setY", "value"), &MouseImage::setY);
 	ClassDB::bind_method(D_METHOD("getX"), &MouseImage::getX);
 	ClassDB::bind_method(D_METHOD("getY"), &MouseImage::getY);
+	ClassDB::bind_method(D_METHOD("setLocation", "x", "y"), &MouseImage::setLocation);
+	ClassDB::bind_method(D_METHOD("setImagePath", "path"), &MouseImage::setImagePath);
+	ClassDB::bind_method(D_METHOD("getPath"), &MouseImage::getPath);
+	ClassDB::bind_method(D_METHOD("updateImage"), &MouseImage::getPath);
+}
 
+void MouseImage::_notification(int i) {
+	if (NOTIFICATION_PROCESS == i) {
+		x = get_global_mouse_position().x;
+		y = get_global_mouse_position().y;
+
+		set_global_position(Point2(x, y));
+
+		
+	}
+	
+	if (NOTIFICATION_READY == i) {
+
+		mPath = "res://assassin.png";
+		updateImage();
+
+	}
 }
--- godot-3.2.2-stable/modules/mouse_image/mouse_image.h
+++ godot-3.2.2-stable/modules/mouse_image/mouse_image.h
@@ -1,22 +1,31 @@
 #pragma once
 
-#include "core/reference.h"
+using namespace std;
 
-class MouseImage : public Reference {
-	GDCLASS(MouseImage, Reference);
+#include <string>
+#include "scene/2d/sprite.h"
+
+class MouseImage : public Sprite {
+	GDCLASS(MouseImage, Sprite);
 
 private:
 	int x, y;
+	String mPath;
 
 protected:
 	static void _bind_methods();
+	void MouseImage::_notification(int i);
 
 public:
-	void setX(int);
-	void setY(int);
-
 	int getX();
 	int getY();
+
+	void setLocation(int x, int y);
+	void setImagePath(String path);
+
+	String getPath();
+
+	void updateImage();
 
 	MouseImage();
 	~MouseImage();
